da090d348881676cc8b62c79a48202e5dab40b8d
Alan Kittani <42450737+alan-kittani@users.noreply.github.com>
2022-07-07 20:00:07 -0500

    Update Class_1.java

--- Alan_Package/Class_1.java
+++ Alan_Package/Class_1.java
@@ -3,6 +3,7 @@ package Alan_Package;
 public class Class_1 {
 
     public static void main(String[] args) {
-        System.out.println("Hi");
+          System.out.println("HELLLO");
+    
     }
 }
